20fa33a004cab08f6481904b821dbf7c77c4315c
Shraddha Chadha <shradhachadha08@gmail.com>
2018-11-18 15:03:55 -0800

    top 10 restaurant

--- top-restaurant/src/app/restaurant-card/restaurant-card.component.ts
+++ top-restaurant/src/app/restaurant-card/restaurant-card.component.ts
@@ -10,11 +10,17 @@ export class RestaurantCardComponent implements OnInit {
 
   @Input() restaurant;
   @Input() cardId;
+  public categories;
+  public checked_stars = 0;
+  public unchecked_stars = 0;
 
   constructor() { }
 
   ngOnInit() {
-
+    this.categories = this.restaurant.categories.split(', ');
+    this.categories.sort();
+    this.checked_stars = Math.floor(this.restaurant.stars);
+    this.unchecked_stars = 5 - this.checked_stars;
   }
 
 }
--- top-restaurant/src/app/top-restaurant.service.ts
+++ top-restaurant/src/app/top-restaurant.service.ts
@@ -12,7 +12,7 @@ export class TopRestaurantService {
   constructor(private http: HttpClient) { }
 
   getTopRestaurants() {
-    return this.http.get('http://127.0.0.1:8080/api/v1/entries/43.712946/-79.632763/5/quarter1/2017');
+    return this.http.get('http://127.0.0.1:8080/api/v1/entries/quarter1/2017');
   }
 
 
