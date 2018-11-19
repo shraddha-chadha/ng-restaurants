214ea748f92490aca251fe20dd90703404e4c834
Shraddha Chadha <shradhachadha08@gmail.com>
2018-11-18 19:02:11 -0800

    list_filters

--- top-restaurant/src/app/app.module.ts
+++ top-restaurant/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -50,6 +51,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
 
